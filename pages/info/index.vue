ed4fd83df8b6fea150b0b6a6757f2b48a569f79a
bkrewer <brnkrewer@gmail.com>
2022-03-17 21:00:04 -0300

    ajustes de estilo e background

--- pages/info/index.vue
+++ pages/info/index.vue
@@ -1,104 +1,121 @@
 <template>
   <section class="info-page page-box">
-    <div class="info-page__basic box-info">
-      <div class="box-info__header">Informações Básicas</div>
-      <div class="box-info__body">
-        <ul>
-          <li>> 40X EXP</li>
-          <li>> Drop Customizado (All drops)</li>
-          <li>> 10 Classes (Até Tier 4)</li>
-          <li>> Level Cap 100</li>
-          <li>> Sistema de Clãs 100% funcionando</li>
-          <li>> 24/7 Bellatra</li>
-          <li>> Suporte para Windows Vista, 7, 8, & 8.1! Alto FPS!</li>
-          <li>> Modo Janela</li>
-          <li>
-            > 3 Coroas de Bellatra: Adicionando HP, MP, STM para um melhor
-            balanceamento!
-          </li>
-          <li>
-            > 3 Coroas do Castelo Abençoado: 3% (Bronze), 5% (Prata), 10%
-            (Ouro)! Exclusivo
-          </li>
-          <li>> Chat Global (Troca)</li>
-          <li>> Barra de rolagem no chat e alerta! Exclusivo</li>
-          <li>> Pets (Cristais de Monstros) não atacam um ao outro</li>
-          <li>> Limite de peso e ouro aumentados!</li>
-          <li>> Baú com 5 páginas</li>
-          <li>> Novo sistema de screenshot!</li>
-        </ul>
+    <div class="info-page__texts">
+      <div class="info-page__basic box-info">
+        <div class="box-info__header">Informações Básicas</div>
+        <div class="box-info__body">
+          <ul>
+            <li>> 40X EXP</li>
+            <li>> Drop Customizado (All drops)</li>
+            <li>> 10 Classes (Até Tier 4)</li>
+            <li>> Level Cap 100</li>
+            <li>> Sistema de Clãs 100% funcionando</li>
+            <li>> 24/7 Bellatra</li>
+            <li>> Suporte para Windows Vista, 7, 8, & 8.1! Alto FPS!</li>
+            <li>> Modo Janela</li>
+            <li>
+              > 3 Coroas de Bellatra: Adicionando HP, MP, STM para um melhor
+              balanceamento!
+            </li>
+            <li>
+              > 3 Coroas do Castelo Abençoado: 3% (Bronze), 5% (Prata), 10%
+              (Ouro)! Exclusivo
+            </li>
+            <li>> Chat Global (Troca)</li>
+            <li>> Barra de rolagem no chat e alerta! Exclusivo</li>
+            <li>> Pets (Cristais de Monstros) não atacam um ao outro</li>
+            <li>> Limite de peso e ouro aumentados!</li>
+            <li>> Baú com 5 páginas</li>
+            <li>> Novo sistema de screenshot!</li>
+          </ul>
+        </div>
+      </div>
+      <div class="info-page__advanced box-info">
+        <div class="box-info__header">Diferenciais</div>
+        <div class="box-info__body">
+          <ul>
+            <li>> Sistema de Craft (Reformulado)! Exclusivo</li>
+            <li>> Nova GUI + UI (HUD) totalmente reforumlada! Exclusivo</li>
+            <li>> Sistema de Alerta com esmaecimento! Exclusivo</li>
+            <li>> Bellatra sem atraso de monstro no respawn! Exclusivo</li>
+            <li>> Suporte completo para W10! Exclusivo</li>
+            <li>
+              > New Bellatra Crowns with HP, MP, STM for a better class balance!
+              Exclusivo
+            </li>
+            <li>> Widescreen suportando (16:9)(16:10) com auto-ajuste!</li>
+            <li>> Câmera em Dungeons desbugada!Exclusivo</li>
+            <li>> Suporte para todas as Resoluções Widescreen!</li>
+            <li>> Engine Gráfica (DirectX 9)! Exclusivo</li>
+            <li>> Tier List automática!</li>
+            <li>> Configurações In-Game 100%, em tempo real (F10)!Exclusivo</li>
+            <li>> Sistema de Skins! Exclusivo</li>
+            <li>> Sistema de Títulos (Top Level) na box do personagem!</li>
+            <li>
+              > Todas as skills refeitas para uma melhor performace e
+              balanceamento! Exclusivo
+            </li>
+            <li>
+              > "Spawn" limpando Itens após um período automaticamente!
+              Exclusivo
+            </li>
+            <li>
+              > Skills das classes mágicas com chance de crítico! Exclusivo
+            </li>
+            <li>> Sistema de Brincos e Cintos!</li>
+            <li>
+              > Sistema de informação de danos! Defesa, Dano, Block, Crítico,
+              Evasão, HP, Mana e Res! Exclusivo
+            </li>
+            <li>> Página do personagem com mais informações!</li>
+            <li>> Rei do PVP (Maior BP)!</li>
+            <li>> VL Rebuff pegando em todos do Grupo!</li>
+            <li>
+              > Sistema de Desafios, apertar "Q"! Diários, repetitivos e fixos!
+            </li>
+            <li>> Arena do Prince! Exclusivo</li>
+            <li>
+              > Status Perfeito (Apertar "CTRL" para visualizar sobre o item não
+              equipado)!
+            </li>
+            <li>> Botão direito para Baú, Mix e Aging!</li>
+            <li>> Bônus Inicial! Exclusivo</li>
+            <li>> Nova Janela para Troca e Grupo! Exclusivo</li>
+            <li>> Modo Guerra (Apertar "K" para ativar)! Exclusivo</li>
+            <li>> Sistema de Trajes 100%!</li>
+            <li>> Core Azul reformulado!</li>
+            <li>> Roleta de Boss! Exclusivo</li>
+            <li>> Shop de Coins e Time dentro do jogo!</li>
+            <li>> Nome do Mix mostrando nos Itens!</li>
+            <li>
+              > "Item do Prince", mostrando caso for de algum Boss, mostrará no
+              Item! Exclusivo
+            </li>
+            <li>> Janela do Aging mostrando a falha! Exclusivo</li>
+            <li>> Sistema de Notícia de Clãs!</li>
+            <li>> Chat do Clã avisa quando o membro está Online!</li>
+            <li>> Tooltip com Hora Digital! Exclusivo</li>
+            <li>> Novos e Exclusivos Itens Premiuns! Exclusivo</li>
+            <li>> Ao apertar "B", seus status sobem de 10 em 10!</li>
+            <li>> Novos NPC's e Mapas!</li>
+            <li>> Experiência ganha mostrando no Alerta! Exclusivo</li>
+            <li>> E muito mais!</li>
+          </ul>
+        </div>
+      </div>
+      <div class="info-page__download">
+        <div class="info-page__download-texts">
+          <h2 class="info-page__download-title">Não perca tempo!</h2>
+          <p class="info-page__download-subtitle">Faça o download e inicie agora mesmo.</p>
+        </div>
+        <NuxtLink to="/download">
+          Download do Jogo
+        </NuxtLink>
       </div>
     </div>
-    <div class="info-page__advanced box-info">
-      <div class="box-info__header">Diferenciais</div>
-      <div class="box-info__body">
-        <ul>
-          <li>> Sistema de Craft (Reformulado)! Exclusivo</li>
-          <li>> Nova GUI + UI (HUD) totalmente reforumlada! Exclusivo</li>
-          <li>> Sistema de Alerta com esmaecimento! Exclusivo</li>
-          <li>> Bellatra sem atraso de monstro no respawn! Exclusivo</li>
-          <li>> Suporte completo para W10! Exclusivo</li>
-          <li>
-            > New Bellatra Crowns with HP, MP, STM for a better class balance!
-            Exclusivo
-          </li>
-          <li>> Widescreen suportando (16:9)(16:10) com auto-ajuste!</li>
-          <li>> Câmera em Dungeons desbugada!Exclusivo</li>
-          <li>> Suporte para todas as Resoluções Widescreen!</li>
-          <li>> Engine Gráfica (DirectX 9)! Exclusivo</li>
-          <li>> Tier List automática!</li>
-          <li>> Configurações In-Game 100%, em tempo real (F10)!Exclusivo</li>
-          <li>> Sistema de Skins! Exclusivo</li>
-          <li>> Sistema de Títulos (Top Level) na box do personagem!</li>
-          <li>
-            > Todas as skills refeitas para uma melhor performace e
-            balanceamento! Exclusivo
-          </li>
-          <li>
-            > "Spawn" limpando Itens após um período automaticamente! Exclusivo
-          </li>
-          <li>> Skills das classes mágicas com chance de crítico! Exclusivo</li>
-          <li>> Sistema de Brincos e Cintos!</li>
-          <li>
-            > Sistema de informação de danos! Defesa, Dano, Block, Crítico,
-            Evasão, HP, Mana e Res! Exclusivo
-          </li>
-          <li>> Página do personagem com mais informações!</li>
-          <li>> Rei do PVP (Maior BP)!</li>
-          <li>> VL Rebuff pegando em todos do Grupo!</li>
-          <li>
-            > Sistema de Desafios, apertar "Q"! Diários, repetitivos e fixos!
-          </li>
-          <li>> Arena do Prince! Exclusivo</li>
-          <li>
-            > Status Perfeito (Apertar "CTRL" para visualizar sobre o item não
-            equipado)!
-          </li>
-          <li>> Botão direito para Baú, Mix e Aging!</li>
-          <li>> Bônus Inicial! Exclusivo</li>
-          <li>> Nova Janela para Troca e Grupo! Exclusivo</li>
-          <li>> Modo Guerra (Apertar "K" para ativar)! Exclusivo</li>
-          <li>> Sistema de Trajes 100%!</li>
-          <li>> Core Azul reformulado!</li>
-          <li>> Roleta de Boss! Exclusivo</li>
-          <li>> Shop de Coins e Time dentro do jogo!</li>
-          <li>> Nome do Mix mostrando nos Itens!</li>
-          <li>
-            > "Item do Prince", mostrando caso for de algum Boss, mostrará no
-            Item! Exclusivo
-          </li>
-          <li>> Janela do Aging mostrando a falha! Exclusivo</li>
-          <li>> Sistema de Notícia de Clãs!</li>
-          <li>> Chat do Clã avisa quando o membro está Online!</li>
-          <li>> Tooltip com Hora Digital! Exclusivo</li>
-          <li>> Novos e Exclusivos Itens Premiuns! Exclusivo</li>
-          <li>> Ao apertar "B", seus status sobem de 10 em 10!</li>
-          <li>> Novos NPC's e Mapas!</li>
-          <li>> Experiência ganha mostrando no Alerta! Exclusivo</li>
-          <li>> E muito mais!</li>
-        </ul>
-      </div>
-    </div>
+        <aside class="info-page__image">
+      <img src="~/assets/images/shy.gif" alt="Monstro shy" />
+    </aside>
   </section>
 </template>
 
